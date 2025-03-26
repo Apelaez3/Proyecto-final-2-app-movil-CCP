a3e8e2ba6d4920628c08fc8c7247dfe400ea7d23
unknown <j.pelaezg@uniandes.edu.co>
2025-03-26 17:55:00 -0500

    login set as home page

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule]
+  imports: [CommonModule, FormsModule, IonicModule,RouterModule]
 })
 export class LoginPage {}
 
