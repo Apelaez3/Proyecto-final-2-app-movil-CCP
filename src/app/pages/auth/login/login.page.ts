2abf8e5082f79aede10735946d2ba6ecb9cbd015
Jader Pelaez <j.pelaezg@uniandes.edu.co>
2025-03-29 15:56:46 -0500

    auth service

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,25 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, FormsModule, IonicModule,RouterModule]
 })
-export class LoginPage {}
+export class LoginPage {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        this.router.navigate(['/home']);  // Redirige a la página principal
+      },
+      error: (error) => {
+        console.error('Error en login:', error);
+        this.errorMessage = 'Usuario o contraseña incorrectos';
+      },
+    });
+  }
+}
 
 
